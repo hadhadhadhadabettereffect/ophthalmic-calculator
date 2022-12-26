a5092b5aca43494a65e4b74661fa66290aee5ce0
eric <ericjbirkeland@gmail.com>
2022-12-25 17:48:27 -1000

    init main.js with first button group functions

---
+++ main.js
@@ -0,0 +1,61 @@
+const input = document.getElementById("input_y");
+const output = document.getElementById("output");
+
+
+document.getElementById("button_a").addEventListener("click", function (event) {
+    requestAnimationFrame(outputCmToDiopters);
+});
+
+document.getElementById("button_b").addEventListener("click", function (event) {
+    requestAnimationFrame(outputInchesToDiopters);
+});
+
+document.getElementById("button_c").addEventListener("click", function (event) {
+    requestAnimationFrame(outputDioptersToCm);
+});
+
+document.getElementById("button_d").addEventListener("click", function (event) {
+    requestAnimationFrame(outputDioptersToInches);
+});
+
+
+function outputCmToDiopters() {
+    const result = cmToDiopters(input.value);
+    output.innerText = result;
+}
+
+function outputInchesToDiopters() {
+    const result = inchesToDiopters(input.value);
+    output.innerText = result;
+}
+
+function outputDioptersToCm() {
+    const result = dioptersToCm(input.value);
+    output.innerText = result;
+}
+
+function outputDioptersToInches() {
+    const result = dioptersToInches(input.value);
+    output.innerText = result;
+}
+
+
+function cmToDiopters(y) {
+    if (y <= 0 || y >= 500) return "input must be between 0 and 500";
+    return (100 / y).toFixed(2);
+}
+
+function inchesToDiopters(y) {
+    if (y <= 0 || y >= 200) return "input must be between 0 and 200";
+    return (100 / (2.54 * y)).toFixed(2);
+}
+
+function dioptersToCm(y) {
+    if (y <= -60 || y >= 60 || y == 0) return "input must be between -60 and 60";
+    return (100 / y).toFixed(2);
+}
+
+function dioptersToInches(y) {
+    if (y <= -60 || y >= 60 || y == 0) return "input must be between -60 and 60";
+    return (100 / (2.54 * y)).toFixed(2);
+}
